f9a65fc0ec5d2262c9d4b2adf0e6c39af15aa6f7
katherinescott <katherinescott@college.harvard.edu>
2018-02-20 20:10:22 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -42,10 +42,10 @@ class LBL(nn.Module):
         #filter out vals where norm > max norm
         to_rescale = Variable(torch.from_numpy(
                 np.where(norms.data.numpy() > max_norm)[0])).cuda()
-        norms = torch.norm(self.embedding_layer(to_rescale), p=2, dim=1).data
+        norms = torch.norm(self.embedding_layer(to_rescale), p=2, dim=1).cuda().data
         scaled = self.embedding_layer(to_rescale).div(
                 Variable(norms.view(len(to_rescale), 1).expand_as(
-                        self.embedding_layer(to_rescale)))).data.cuda()
+                        self.embedding_layer(to_rescale)))).cuda().data
         self.embedding_layer.weight.data[to_rescale.long().data] = scaled
 
     def forward(self, context_words):

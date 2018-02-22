a33f9849ec55739a554039d40f253d57fe7f151f
katherinescott <katherinescott@college.harvard.edu>
2018-02-21 20:07:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def train(model, optimizer, data_iter, text_field, args):
     batch_idx = 0
     for batch in data_iter:
         context = torch.transpose(batch.text, 0, 1)
-        target = batch.target[-1, :]
+        target = batch.target[-1, :].cuda()
         batch_size = context.size(0)
         # zero out gradients
         optimizer.zero_grad()

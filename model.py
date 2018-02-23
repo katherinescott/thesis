4070171d17bea555d1bc2230db9a7aa152cc76ad
katherinescott <katherinescott@college.harvard.edu>
2018-02-23 12:00:34 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -37,14 +37,14 @@ class LBL(nn.Module):
 	#torch.norm(input tensor, p=2, dim) p = exponent val in norm formulation, dim = dimension to reduce
     #make sure weights never exceeds a certain threshold 
     def max_norm_embedding(self, max_norm=1):
-        norms = torch.norm(self.embedding_layer.weight, p=2, dim=1).cuda()
+        norms = torch.norm(self.embedding_layer.weight, p=2, dim=1)
         #filter out vals where norm > max norm
         to_rescale = Variable(torch.from_numpy(
-                np.where(norms.data.cpu().numpy() > max_norm)[0])).cuda()
-        norms = torch.norm(self.embedding_layer(to_rescale).cuda(), p=2, dim=1).cuda().data
+                np.where(norms.data.cpu().numpy() > max_norm)[0]))
+        norms = torch.norm(self.embedding_layer(to_rescale), p=2, dim=1).data
         scaled = self.embedding_layer(to_rescale).div(
                 Variable(norms.view(len(to_rescale), 1).expand_as(
-                        self.embedding_layer(to_rescale)))).data.cuda()
+                        self.embedding_layer(to_rescale)))).data
         self.embedding_layer.weight.data[to_rescale.long().data] = scaled
 
 

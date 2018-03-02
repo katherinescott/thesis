0329d0227b00fdeb776d8d624e61f159dbd7971c
katherinescott <katherinescott@college.harvard.edu>
2018-03-02 10:10:57 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -115,7 +115,7 @@ class CondCopy(nn.Module):
         #norms = torch.unsqueeze(norms, 0) #or squeeze?
         #filter out vals where norm > max norm
         to_rescale = Variable(torch.from_numpy(
-                np.where(norms.data.cpu().numpy() > max_norm)[0]))
+                np.where(norms.data.cpu().numpy() > max_norm)[1]))
         norms = torch.norm(self.embedding_layer(to_rescale), p=2, dim=1).data
         scaled = self.embedding_layer(to_rescale).div(
                 Variable(norms.view(len(to_rescale), 1).expand_as(

bc9849d15ac8f38d03d66ffc2686ea387936ba30
katherinescott <katherinescott@college.harvard.edu>
2018-03-08 10:01:54 -0500

    Add files via upload

--- main2.py
+++ main2.py
@@ -26,14 +26,14 @@ def train(model, optimizer, data_iter, text_field, args):
     iter_len = len(data_iter)
     batch_idx = 0
     for batch in data_iter:
-        context = torch.transpose(batch.text, 0, 1)
+        context = torch.transpose(batch.text[:, -5:], 0, 1)
         target = (batch.target[-1, :]).cuda()
 
         batch_size = context.size(0)
 
         #pointer_vocab = text_field.build_vocab(batch.text, vectors=torchtext.vocab.GloVe(name='6B', dim=100))
 
-        words_before = (batch.target[0:-1, :]).cuda() 
+        words_before = torch.transpose(batch.text[:, :-5], 0, 1).cuda()
 
         # zero out gradients
         optimizer.zero_grad()
@@ -44,9 +44,20 @@ def train(model, optimizer, data_iter, text_field, args):
         # calculate loss
         #pdb.set_trace()
         loss = loss_function_avg(shortlist, target)
-        loss += loss_function_avg(pointer, words_before)
         total_loss += loss_function_tot(shortlist, target).data.cpu().numpy()[0]
-        total_loss += loss_function_tot(pointer, words_before).data.cpu().numpy()[0]
+
+        #50 context words, use last 5 as context, previous as pointers then look in the 50 context words and see if target was in them, find that index,
+        #then index into 
+        indices = []
+        for i in range(1, words_before.size(1)):
+            if words_before[:,i] == target
+            indices.append(i)
+
+        for i in range(len(indices)):
+            loss += loss_function_avg(pointer, words_before[:,indices[i]])
+            total_loss += loss_function_tot(pointer, words_before[:,indices[i]]).data.cpu().numpy()[0]
+        
+
         data_size += batch_size
         # calculate gradients
         loss.backward()

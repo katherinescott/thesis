fd826de0a589cb80553e82afdec1f2964827cbe7
katherinescott <katherinescott@college.harvard.edu>
2018-03-22 15:49:18 -0400

    Add files via upload

--- main2.py
+++ main2.py
@@ -26,7 +26,7 @@ def train(model, model2, optimizer, optimizer2, data_iter, text_field, args):
     iter_len = len(data_iter)
     batch_idx = 0
     for batch in data_iter:
-        context = torch.transpose(batch.text, 0, 1)
+        context = torch.transpose(batch.text, 0, 1).cuda()
         target = (batch.target[-1, :]).cuda()
 
         batch_size = context.size(0)
@@ -112,7 +112,7 @@ def evaluate(model, model2, data_iter, text_field, args):
     batch_idx = 0
     for batch in data_iter:
         context = torch.transpose(batch.text, 0, 1)
-        target = (batch.target[-1, :]).cuda()
+        target = (batch.target[-1, :]) #.cuda()
         batch_size = context.size(0)
 
         words_before = context[:,:-5].cuda() #[:, :-5]

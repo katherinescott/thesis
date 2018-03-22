2e8dc63552ec021ad95483403fc5206fc48bc1d8
katherinescott <katherinescott@college.harvard.edu>
2018-03-22 10:27:31 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ parser = argparse.ArgumentParser()
 # required arguments
 parser.add_argument("--context_size",
                     help="number of words taken into account in context",
-                    type=int, default=5)
+                    type=int, default=50)
 parser.add_argument("--epochs", help="number of epochs",
                     type=int, default=20)
 parser.add_argument("--batch_size", help="batching size",
                     type=int, default=128)
 parser.add_argument("--GPU",
                     help="use GPU optimizer (1 to use GPU, 0 for no GPU)",
-                    type=int, default=0)
+                    type=int, default=1)
 parser.add_argument("--mode", help="specify test, validate, or train",
                     type=str, default="test")
 parser.add_argument("--lr", help="learning rate",

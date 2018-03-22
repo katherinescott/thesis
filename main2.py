075a3f2444aff73237cbc7028fd9f3e2cac678d7
katherinescott <katherinescott@college.harvard.edu>
2018-03-22 17:56:41 -0400

    Add files via upload

--- main2.py
+++ main2.py
@@ -23,7 +23,7 @@ def train(model, optimizer, data_iter, text_field, args):
     total_loss = 0
     data_size = 0
 
-    print(text_field.vocab.itos)
+    print(text_field.vocab.stoi[0])
     iter_len = len(data_iter)
     batch_idx = 0
     for batch in data_iter:

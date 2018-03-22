7014161c755a37c2a61f5d295e4b2a06b607c33c
katherinescott <katherinescott@college.harvard.edu>
2018-03-22 15:53:42 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ parser.add_argument("--batch_size", help="batching size",
                     type=int, default=128)
 parser.add_argument("--GPU",
                     help="use GPU optimizer (1 to use GPU, 0 for no GPU)",
-                    type=int, default=1)
+                    type=int, default=0)
 parser.add_argument("--mode", help="specify test, validate, or train",
                     type=str, default="test")
 parser.add_argument("--lr", help="learning rate",

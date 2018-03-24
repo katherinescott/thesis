0b79c94b4b16df952dac7e6581c90ebebce57799
katherinescott <katherinescott@college.harvard.edu>
2018-03-24 13:47:12 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -204,7 +204,7 @@ class CondCopy(nn.Module):
 
             point_scores.append(prob_ptr + a[-1].unsqueeze(1))
 
-            print(torch.log(torch.cat(probs)))
+            print(torch.log(probs)
 
         return torch.log(torch.cat(probs).view(-1, self.vocab_size)), torch.log(torch.cat(point_scores).view(-1, self.vocab_size))
 

a2f3effd852070209b6eb67ae58799101b72f49e
katherinescott <katherinescott@college.harvard.edu>
2018-03-23 21:55:19 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -162,7 +162,7 @@ class CondCopy(nn.Module):
         probs = []
 
         cumulate = torch.zeros((length, self.batch_size, self.vocab_size))
-        cumulate.scatter_(1, context_words.view(self.batch_size, length, 1).contiguous(), 1.0)
+        cumulate.scatter_(1, context_words.unsqueeze(2), 1.0)
 
         point_scores = []
         

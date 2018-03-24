de8af4ece38f1858c6e7e6ead63e6d6fd04a1d06
katherinescott <katherinescott@college.harvard.edu>
2018-03-23 22:58:09 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -169,8 +169,8 @@ class CondCopy(nn.Module):
         for i in range(length):
             embeddings = self.embedding_layer(context_words.transpose(0,1)[i]) #now its contxt x batch
             assert embeddings.size() == \
-            (self.batch_size, int(self.context_size/10), self.hidden_size)
-             
+            (self.batch_size, 1, self.hidden_size)
+
             cvecs = self.context_layer(embeddings.view(self.batch_size, int(self.context_size/10) * self.hidden_size))
             q = F.tanh(self.output_location(cvecs))
 

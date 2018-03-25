911be023db76b006c56c231d5c5a942079b4a8b9
katherinescott <katherinescott@college.harvard.edu>
2018-03-25 14:02:47 -0400

    Add files via upload

--- main2.py
+++ main2.py
@@ -139,7 +139,7 @@ def evaluate(model, data_iter, text_field, args):
         #print(pred_words)
         print(torch.max(shortlist.data[-1,:]))
         print((shortlist == torch.max(shortlist.data[-1,:])).nonzero().data.tolist()[0][1])
-        print(text_field.vocab.itos(0))
+        print(text_field.vocab.itos[(shortlist == torch.max(shortlist.data[-1,:])).nonzero().data.tolist()[0][1]])
         # calculate total loss
         loss = loss_function_tot(shortlist, target)  # loss is already averaged
 

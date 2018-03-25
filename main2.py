69375278c76f62b21db385f20ce33690701e66df
katherinescott <katherinescott@college.harvard.edu>
2018-03-25 18:44:12 -0400

    Add files via upload

--- main2.py
+++ main2.py
@@ -104,7 +104,7 @@ def train(model, optimizer, data_iter, text_field, args):
         # enforce the max_norm constraint
         #model.max_norm_embedding()
         # skip the last batch
-        if batch_idx >= iter_len - 2:
+        if batch_idx >=1: # iter_len - 2:
             break
 
         batch_idx += 1
@@ -180,7 +180,7 @@ def evaluate(model, data_iter, text_field, args):
         data_size += batch_size
 
         # skip last batch
-        if batch_idx >=1: #iter_len - 2:
+        if batch_idx >= iter_len - 2:
             break
 
         batch_idx += 1

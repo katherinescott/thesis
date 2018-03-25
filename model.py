af052e9280a3995787414d9a9ce3da9ba6425dd1
katherinescott <katherinescott@college.harvard.edu>
2018-03-25 14:24:26 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -111,11 +111,11 @@ class CondCopy(nn.Module):
         self.max_norm_embedding()
         # C in the paper // nn.Linear (in features, out features) *doesn't learn additive bias
         self.context_layer = nn.Linear(
-                self.hidden_size * int(self.context_size/10),
+                self.hidden_size * int(self.context_size),
                 self.hidden_size, bias=False)
 
         self.context_layer2 = nn.Linear(
-                self.hidden_size* int(self.context_size/10), 
+                self.hidden_size* int(self.context_size), 
                 self.hidden_size, bias=False)
         # dot product + bias in the paper
         self.output_shortlist =\
@@ -158,7 +158,7 @@ class CondCopy(nn.Module):
 
     def forward(self, context_words):
         self.batch_size = context_words.size(0)
-        assert context_words.size(1) == int(self.context_size/10), \
+        assert context_words.size(1) == int(self.context_size), \
             "context_words.size()=%s | context_size=%d" % \
             (context_words.size(), self.context_size)
 
@@ -166,22 +166,22 @@ class CondCopy(nn.Module):
         embeddings = self.embedding_layer(context_words)
         # sanity check
         assert embeddings.size() == \
-            (self.batch_size, int(self.context_size/10), self.hidden_size)
+            (self.batch_size, int(self.context_size), self.hidden_size)
 
         embeds2 = F.relu(embeddings)
 
         assert embeds2.size() == \
-            (self.batch_size, int(self.context_size/10), self.hidden_size)
+            (self.batch_size, int(self.context_size), self.hidden_size)
 
         #get context vectors
         context_vectors = self.context_layer(embeddings.view(
-                self.batch_size, int(self.context_size/10) * self.hidden_size))
+                self.batch_size, int(self.context_size) * self.hidden_size))
         context_vectors = self.dropout(context_vectors)
         assert context_vectors.size() == (self.batch_size, self.hidden_size)
 
         #context vectors for pointer
         context_vecs2 = self.context_layer2(embeds2.view(
-                self.batch_size, int(self.context_size/10) * self.hidden_size))
+                self.batch_size, int(self.context_size) * self.hidden_size))
         context_vecs2 = self.dropout(context_vecs2)
         assert context_vecs2.size() == (self.batch_size, self.hidden_size)
         
@@ -210,15 +210,15 @@ class CondCopy(nn.Module):
 
         location_outputs = torch.bmm(embeddings, l_cvecs.view(self.batch_size, self.hidden_size, 1).contiguous())
 
-        assert location_outputs.size() == (self.batch_size, int(self.context_size/10), 1)
+        assert location_outputs.size() == (self.batch_size, int(self.context_size), 1)
 
         location_outputs = torch.squeeze(location_outputs)
 
-        assert location_outputs.size() == (self.batch_size, int(self.context_size/10))
+        assert location_outputs.size() == (self.batch_size, int(self.context_size))
 
         l_outputs = F.softmax(location_outputs, dim=1)
 
-        assert l_outputs.size() == (self.batch_size, int(self.context_size/10))
+        assert l_outputs.size() == (self.batch_size, int(self.context_size))
 
         #6) Now you need to somehow combine the two distributions you formed in steps (3) and (5). I guess the easiest approach is to have another 
     #distribution that tells you whether you copied or not. Then, p(word5 | ctx) = p(copied) * pointer_probability_of_word5 + (1 - p(copied)) * probability_of_word5_from_step3.  
